bf655d79901f8dcefc66ec0f07788b9f8d54a1f3
Dermot <dermot.osullivan@cba.com.au>
2018-02-16 15:12:56 +1100

    Initial commit

---
+++ UIDynamics.Showcase/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  UIDynamics.Showcase
+//
+//  Created by Dermot on 16/2/18.
+//  Copyright © 2018 Rocky Desk. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
