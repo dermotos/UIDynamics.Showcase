6f617872d746f46ec3364930d7e11498fef4c210
Dermot O Sullivan <me@dermotos.com>
2018-02-19 07:52:01 +1100

    Saturday work

--- UIDynamics.Showcase/FirstViewController.swift
+++ UIDynamics.Showcase/FirstViewController.swift
@@ -11,8 +11,16 @@ import UIKit
 class FirstViewController: UIViewController {
     
     var animator :UIDynamicAnimator!
-    let gravitationalConstant = 9.8
-    let gravity = UIGravityBehavior()
+    
+    let gravitationalConstant = 0.4
+    let elasticConstant :CGFloat = 0.9
+    
+    
+    var gravity: UIGravityBehavior!
+    var collision :UICollisionBehavior!
+    var elasticity :UIDynamicItemBehavior!
+    
+    
     
 
     override func viewDidLoad() {
@@ -27,19 +35,55 @@ class FirstViewController: UIViewController {
     
     private func setupEnvironment() {
         animator = UIDynamicAnimator(referenceView: view)
+        
+        collision = UICollisionBehavior()
+        collision.translatesReferenceBoundsIntoBoundary = true
+        
+        gravity = UIGravityBehavior()
         gravity.gravityDirection = CGVector(dx: 0, dy: gravitationalConstant)
         
-        animator.addBehavior(gravity)
+        elasticity = UIDynamicItemBehavior()
+        elasticity.elasticity = elasticConstant
         
-        let frame = CGRect(x: 50, y: 50, width: 50, height: 50)
+        animator.addBehavior(collision)
+        animator.addBehavior(gravity)
+        animator.addBehavior(elasticity)
+    }
+    
+    private func addBox(atPosition position:CGPoint, withColor color:UIColor) -> UIView {
+        let boxSize = CGSize(width: 150, height: 150)
+        let frame = CGRect(origin: position, size: boxSize)
         let aBox = UIView(frame: frame)
-        aBox.center = view.center
-        aBox.backgroundColor = .red
+        aBox.backgroundColor = color
         view.addSubview(aBox)
+        return aBox
+    }
+    
+    private func addBasePhysics(to view:UIView) {
+        gravity.addItem(view)
+        collision.addItem(view)
+    }
+    
+    private func addElasticity(to view:UIView) {
+        elasticity.addItem(view)
+    }
+    
+    
+    
+    private func resetScene() {
+        
+        
     }
 
     @IBAction func didTapStart(_ sender: UIButton) {
-        
+        let boxOnePosition = CGPoint(x: 10, y: 10)
+        let boxOne = addBox(atPosition: boxOnePosition, withColor: .red)
+        addBasePhysics(to: boxOne)
+        addElasticity(to: boxOne)
+        //TODO: Add first box, then second box along with a barrier, then gravity, then push them towards each other.
+        //Second view controller is orbit
+        //Third is real world example, perhaps a table view with a UIDynamics transition animator
+        //fourth if I've time is a name picker
     }
     
     @IBAction func didTapReset(_ sender: UIButton) {
